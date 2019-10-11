759f31ab90744ad5d5555491918a292f3f394ba8
Leonid Shamis <leonid.shamis@gmail.com>
2019-10-11 14:40:55 -0700

    call_soon_threadsafe

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
+
+print('importing a0...', file=sys.stderr)
 import a0
+print('importing aiohttp...', file=sys.stderr)
 from aiohttp import web, WSMsgType
+print('importing aiohttp_cors...', file=sys.stderr)
 import aiohttp_cors
+print('importing others...', file=sys.stderr)
 import asyncio
 import base64
 import json
 import os
+import sys
+import threading
+
+print('starting...', file=sys.stderr)
 
 class Namespace:
     def __init__(self, **kwargs):
@@ -22,6 +32,7 @@ class Namespace:
 
     def __getattr__(self, name):
         return self.__dict__.get(name, None)
+
 
 # fetch("http://${api_addr}/api/ls")
 # .then((r) => { return r.text() })
@@ -88,42 +99,76 @@ async def pub_handler(request):
 #     ... evt.data ...
 # }
 async def sub_handler(request):
+    print('sub_handler', request, file=sys.stderr)
     ws = web.WebSocketResponse()
     await ws.prepare(request)
+    print('sub_handler prep', file=sys.stderr)
 
     ns = Namespace(loop = asyncio.get_event_loop())
+    ns.loop.set_debug(True)
+    ns.close = False
+    ns.lock = threading.Lock()
 
-    async for msg in ws:
-        if msg.type == WSMsgType.TEXT:
-            if msg.data == 'close':
+    def handle_exceptions(fut):
+        ex = fut.exception()
+        if ex is not None:
+            print('exception:', ex, file=sys.stderr)
+            ns.close = True
+
+    try:
+        async for msg in ws:
+            if ns.close:
                 break
-            cmd = json.loads(msg.data)
-            tm = a0.TopicManager('''{{
-                "container": "{container}",
-                "subscriber_maps": {{
-                    "topic": {{
-                        "container": "{container}",
-                        "topic": "{topic}"
+            print('sub msg', msg, file=sys.stderr)
+            if msg.type == WSMsgType.TEXT:
+                print('sub msg txt', file=sys.stderr)
+                if msg.data == 'close':
+                    break
+                print('parsing json', file=sys.stderr)
+                cmd = json.loads(msg.data)
+                print('cmd', cmd, file=sys.stderr)
+                tm = a0.TopicManager('''{{
+                    "container": "{container}",
+                    "subscriber_maps": {{
+                        "topic": {{
+                            "container": "{container}",
+                            "topic": "{topic}"
+                        }}
                     }}
-                }}
-            }}'''.format(container=cmd['container'], topic=cmd['topic']))
-            init_ = {
-                'OLDEST': a0.INIT_OLDEST,
-                'MOST_RECENT': a0.INIT_MOST_RECENT,
-                'AWAIT_NEW': a0.INIT_AWAIT_NEW,
-            }[cmd['init']]
-            iter_ = {
-                'NEXT': a0.ITER_NEXT,
-                'NEWEST': a0.ITER_NEWEST,
-            }[cmd['iter']]
-            def callback(pkt):
-                asyncio.ensure_future(ws.send_json({
-                    'headers': pkt.headers,
-                    'payload': base64.b64encode(pkt.payload).decode('utf-8'),
-                }), loop=ns.loop)
-            ns.sub = a0.Subscriber(tm.subscriber_topic('topic'), init_, iter_, callback)
-        elif msg.type == WSMsgType.ERROR:
-            break
+                }}'''.format(container=cmd['container'], topic=cmd['topic']))
+                init_ = {
+                    'OLDEST': a0.INIT_OLDEST,
+                    'MOST_RECENT': a0.INIT_MOST_RECENT,
+                    'AWAIT_NEW': a0.INIT_AWAIT_NEW,
+                }[cmd['init']]
+                iter_ = {
+                    'NEXT': a0.ITER_NEXT,
+                    'NEWEST': a0.ITER_NEWEST,
+                }[cmd['iter']]
+                print('init', init_, file=sys.stderr)
+                print('iter', iter_, file=sys.stderr)
+                def callback(pkt_view):
+                    print('off thread callback', file=sys.stderr)
+                    ns.lock = threading.Lock()
+                    ns.lock.acquire()
+                    def cb_helper(pkt):
+                        print('on thread callback', file=sys.stderr)
+                        fut = asyncio.ensure_future(ws.send_json({
+                            'headers': pkt.headers,
+                            'payload': base64.b64encode(pkt.payload).decode('utf-8'),
+                        }), loop=ns.loop)
+                        fut.add_done_callback(lambda t: ns.lock.release())
+                        print('ensured', file=sys.stderr)
+                    ns.loop.call_soon_threadsafe(cb_helper, a0.Packet(pkt_view))
+                    ns.lock.acquire()
+                print('making sub', file=sys.stderr)
+                ns.sub = a0.Subscriber(tm.subscriber_topic('topic'), init_, iter_, callback)
+                print('made sub', file=sys.stderr)
+            elif msg.type == WSMsgType.ERROR:
+                break
+    except Exception as e:
+        print(f'WS unrecoverable exception: {type(e)}: {e}', file=sys.stderr)
+
 
 # fetch("http://${api_addr}/api/rpc", {
 #     method: "POST",
@@ -173,13 +218,16 @@ async def rpc_handler(request):
     return web.Response(text=json.dumps(await fut))
 
 
+print('creating app', file=sys.stderr)
 app = web.Application()
+print('adding routes', file=sys.stderr)
 app.add_routes([web.get('/api/ls', ls_handler),
                 web.post('/api/ls', ls_handler),
                 web.post('/api/pub', pub_handler),
                 web.get('/api/sub', sub_handler),
                 web.post('/api/rpc', rpc_handler)])
 
+print('adding cors', file=sys.stderr)
 cors = aiohttp_cors.setup(app, defaults={
     "*": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
@@ -190,4 +238,7 @@ cors = aiohttp_cors.setup(app, defaults={
 for route in list(app.router.routes()):
     cors.add(route)
 
+print('running app', file=sys.stderr)
 web.run_app(app, port=24880)
+print('app completed', file=sys.stderr)
+
