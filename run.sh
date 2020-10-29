495f8dea411125896a4e9c7763f57dcb5d542086
Leonid Shamis <lshamis@thirdwave.ai>
2020-10-28 18:55:40 -0700

    Add optional scheduler to sub

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+docker build -t alephzero_api .
+
 # TODO(lshamis): Take ipc container as arg.
 
 docker run --rm -it --name=a0_api --ipc=host -p 24880:24880 alephzero_api
