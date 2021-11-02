57afc35f617ad1be0b3998908bc1ca1f2a3499d8
Leonid Shamis <leonid.shamis@gmail.com>
2021-11-01 19:09:55 -0700

    rm dbg

--- test/run.sh
+++ test/run.sh
@@ -3,14 +3,12 @@ set -e
 cd "$(dirname "$0")"
 
 docker build \
-    --no-cache \
     -t alephzero/api:cov \
     --build-arg=mode=cov \
     -f ../Dockerfile \
     ..
 
 docker build \
-    --no-cache \
     -t alephzero/api_test \
     -f ./Dockerfile \
     ..
