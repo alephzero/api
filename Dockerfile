75a7a132642623ab975e1c3e5e9522ae4fc1cd37
Leonid Shamis <leonid.shamis@gmail.com>
2022-03-06 19:33:16 -0800

    Allow init=seq

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN apt update && DEBIAN_FRONTEND="noninteractive" apt install -y \
 
 RUN mkdir -p /alephzero && \
     cd /alephzero && \
-    git clone --depth 1 https://github.com/alephzero/alephzero.git && \
+    git clone --depth 1 --recursive --shallow-submodules https://github.com/alephzero/alephzero.git && \
     cd /alephzero/alephzero && \
     make install -j
 
---
+++ include/a0/api/scope.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+namespace a0::api {
+
+std::shared_ptr<void> scope_guard(std::function<void()> fn) {
+  return std::shared_ptr<void>((void*)1, [fn](void*) { fn(); });
+}
+
+std::shared_ptr<void> scope_unlock_transport(a0_transport_locked_t tlk) {
+  a0_transport_unlock(tlk);
+  return scope_guard([tlk]() {
+    a0_transport_locked_t unused;
+    a0_transport_lock(tlk.transport, &unused);
+  });
+}
+
+}  // a0::api
